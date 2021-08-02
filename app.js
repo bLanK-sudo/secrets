ac1d95883218312913f4b0819019da481e9309fb
Manish <manish.unofficial993@gmail.com>
2021-08-02 09:26:40 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 
-mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true, useUnifiedTopology:true})
+mongoose.connect('mongodb+srv://admin-manish:admin-manish@cluster0.drcsj.mongodb.net/userDB', {useNewUrlParser: true, useUnifiedTopology:true})
 mongoose.set('useCreateIndex', true)
 const {Schema} = mongoose
 const userSchema = new Schema({
